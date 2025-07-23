be66b75f2abcb58eb76afe2715042cab17df6906
Edu Lima <eduefi@gmail.com>
2025-07-23 11:15:26 +0200

    use 'amount' term instead of 'value'

--- app/routes.py
+++ app/routes.py
@@ -4,8 +4,8 @@ from flask import current_app as app
 @app.route('/transactions', methods=['GET'])
 def get_transactions():
     transactions = [
-        {"name": "dinner", "value": 30.0},
-        {"name": "supermarket", "value": 70.0}
+        {"name": "dinner", "amount": 30.0},
+        {"name": "supermarket", "amount": 70.0}
     ]
 
     return jsonify(transactions)
