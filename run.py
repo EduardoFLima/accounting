bcb02ad12401f8d564edcffdde9c14e3000222fe
Edu Lima <eduefi@gmail.com>
2025-07-24 11:45:07 +0200

    print port when executing locally

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 from app import create_app
+import os
 
 app = create_app()
 
+# just locally, not executed by container
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    print(f'listening to port {port}')
+
     app.run(host='0.0.0.0', port=5000, debug=True)
