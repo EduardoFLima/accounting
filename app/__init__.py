a4eefe8b3b1d9f8c8443b107a953294e40f65484
Edu Lima <eduefi@gmail.com>
2025-07-24 14:29:02 +0200

    add database support

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
 
+    db_uri = str(os.environ.get("SQLALCHEMY_DATABASE_URI", 'postgresql://postgres:postgres@localhost:5432/accounting'))
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+
     with app.app_context():
         from . import routes
+        db.create_all()
 
     return app
